f2653cf65a2ecce853fe964139d86c3082ba43d6
Mathias Dietrich <mdietrich@1153-MacBook-Pro.local>
2018-10-05 13:28:23 -0700

    init

---
+++ src/main/Java/org/test/App.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
